a27d82b6b3a84392995e3ed4b6a6975c43a4b333
Seobs <subi88@naver.com>
2020-05-28 21:09:37 +0900

    first commit

---
+++ android/app/src/main/kotlin/com/example/maybe_dice2/MainActivity.kt
@@ -0,0 +1,6 @@
+package idiotlabs.maybe_dice2
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,223 @@
+import 'dart:math';
+
+import 'package:firebase_admob/firebase_admob.dart';
+import 'package:firebase_analytics/firebase_analytics.dart';
+import 'package:firebase_analytics/observer.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
+import 'package:flutter/material.dart';
+import 'package:positioned_tap_detector/positioned_tap_detector.dart';
+
+void main() {
+  Crashlytics.instance.enableInDevMode = true;
+
+  FlutterError.onError = Crashlytics.instance.recordFlutterError;
+
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  static FirebaseAnalytics analytics = FirebaseAnalytics();
+  static FirebaseAnalyticsObserver observer = FirebaseAnalyticsObserver(analytics: analytics);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Maybe Dice',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      navigatorObservers: <NavigatorObserver>[observer],
+      home: MyHomePage(
+        title: '주사위 굴리기겠지...2',
+        analytics: analytics,
+        observer: observer,
+      ),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title, this.analytics, this.observer}) : super(key: key);
+
+  final String title;
+  final FirebaseAnalytics analytics;
+  final FirebaseAnalyticsObserver observer;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState(analytics, observer);
+}
+
+class _MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin {
+  _MyHomePageState(this.analytics, this.observer);
+
+  int diceNumber1 = 1;
+  int diceNumber2 = 1;
+
+  String _gesture = "";
+  TapPosition _position = TapPosition(Offset.zero, Offset.zero);
+
+  AnimationController dice1AnimationController;
+  AnimationController dice2AnimationController;
+
+  final FirebaseAnalyticsObserver observer;
+  final FirebaseAnalytics analytics;
+  String _message = '';
+
+  BannerAd myBanner = BannerAd(
+    // Replace the testAdUnitId with an ad unit id from the AdMob dash.
+    // https://developers.google.com/admob/android/test-ads
+    // https://developers.google.com/admob/ios/test-ads
+    adUnitId: 'ca-app-pub-8206166796422159/2005131170',
+    size: AdSize.smartBanner,
+    listener: (MobileAdEvent event) {
+      print("BannerAd event is $event");
+    },
+  );
+
+  @override
+  void initState() {
+    super.initState();
+
+    dice1AnimationController = AnimationController(duration: const Duration(milliseconds: 500), vsync: this);
+    dice2AnimationController = AnimationController(duration: const Duration(milliseconds: 500), vsync: this);
+
+    FirebaseAdMob.instance.initialize(appId: FirebaseAdMob.testAppId);
+
+    myBanner
+    // typically this happens well before the ad is shown
+      ..load()
+      ..show(
+        // Positions the banner ad 60 pixels from the bottom of the screen
+        anchorOffset: 40.0,
+        // Positions the banner ad 10 pixels from the center of the screen to the right
+        horizontalCenterOffset: 0.0,
+        // Banner Position
+        anchorType: AnchorType.bottom,
+      );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      drawer: Drawer(),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Row(mainAxisAlignment: MainAxisAlignment.center, children: <Widget>[
+              AnimatedBuilder(
+                animation: dice1AnimationController,
+                child: Image.asset('images/dice' + diceNumber1.toString() + '.png'),
+                builder: (BuildContext context, Widget _widget) {
+                  diceNumber1 = Random().nextInt(5) + 1;
+
+                  if (dice1AnimationController.value == 1) {
+                    if (_position.relative.dx < 30 && _position.relative.dy < 30)
+                      diceNumber1 = Random().nextInt(2) + 4;
+                    else if (_position.relative.dx > 285 && _position.relative.dy > 50)
+                      diceNumber1 = Random().nextInt(2) + 1;
+                  }
+
+                  return Image.asset('images/dice' + diceNumber1.toString() + '.png');
+                },
+              ),
+              Padding(padding: EdgeInsets.all(10.0)),
+              AnimatedBuilder(
+                animation: dice2AnimationController,
+                builder: (BuildContext context, Widget _widget) {
+                  diceNumber2 = Random().nextInt(5) + 1;
+
+                  if (dice2AnimationController.value == 1) {
+                    if (_position.relative.dx < 30 && _position.relative.dy < 30)
+                      diceNumber2 = Random().nextInt(3) + 4;
+                    else if (_position.relative.dx > 285 && _position.relative.dy > 50)
+                      diceNumber2 = Random().nextInt(3) + 1;
+                  }
+
+                  return Image.asset('images/dice' + diceNumber2.toString() + '.png');
+                },
+              ),
+            ]),
+            const SizedBox(height: 40),
+            PositionedTapDetector(
+              onTap: _onTap,
+              onDoubleTap: _onDoubleTap,
+              onLongPress: _onLongPress,
+              child: Container(
+                width: 315.0,
+                height: 80.0,
+                decoration: BoxDecoration(
+                  color: Colors.white70,
+                  borderRadius: BorderRadius.all(Radius.circular(15)),
+                  boxShadow: [
+                    BoxShadow(
+                        color: Colors.black38,
+                        spreadRadius: 2,
+                        blurRadius: 2.0,
+                        offset: Offset(1, 2)),
+                  ],
+                ),
+                child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Text(
+                        '굴러라!',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 41,
+                        ),
+                      )
+                    ]),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    dice1AnimationController.dispose();
+    dice2AnimationController.dispose();
+
+    super.dispose();
+  }
+
+  void _onTap(TapPosition position) => _updateState('single tap', position);
+
+  void _onDoubleTap(TapPosition position) => _updateState('double tap', position);
+
+  void _onLongPress(TapPosition position) => _updateState('long press', position);
+
+  void _updateState(String gesture, TapPosition position) {
+    _clickRollDice();
+
+    setState(() {
+      _gesture = gesture;
+      _position = position;
+    });
+
+    dice1AnimationController.forward(from: 0.0);
+    dice2AnimationController.forward(from: 0.0);
+  }
+
+  void setMessage(String message) {
+    setState(() {
+      _message = message;
+    });
+  }
+
+  Future<void> _clickRollDice() async {
+    await analytics.logEvent(
+      name: 'clickRollDice',
+      parameters: <String, dynamic>{
+      },
+    );
+    setMessage('logEvent succeeded');
+  }
+}
